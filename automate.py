710b27787e0627386ce03c0a6e731dd9d67b9242
Ranjani <ranjanimk3@gmail.com>
2021-01-05 23:09:15 +0100

    Create  automate.py
    WIP

---
+++ automate.py
@@ -0,0 +1,15 @@
+import os
+from bs4 import BeautifulSoup
+
+printMsg1()
+
+current_dir=r'C:/Apache24/htdocs/example'
+for item in os.listdir(current_dir):
+	if not os.path.isfile(item):
+		for file in os.listdir(item):
+			if file.endswith((".html", "_files")):
+                	soup = BeautifulSoup(r.text, 'html.parser')
+				Table = soup.find_all('table', attrs={'class':'table table-bordered'})
+				print(Table)
+
+printMsg2()
