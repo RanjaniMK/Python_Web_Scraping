70fbb82384d1e7bc35d4baaf7c028ed71a65e1cd
Ranjani <ranjanimk3@gmail.com>
2021-01-05 22:42:10 +0100

    Create Begin.py

---
+++ Begin.py
@@ -0,0 +1,13 @@
+def printMsg1():
+    str = """<!DOCTYPE html>
+<html>
+<head>
+<title>Python Automation for class-* html files</title>
+</head>
+<body>
+
+<h1>Extracting only tables from all class--*html files present</h1>
+<p>START</p>
+"""
+    print(str)
+printMsg1()
